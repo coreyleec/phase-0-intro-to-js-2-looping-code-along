e254ee370246ce2fc273003d73804c69f9ae22d2
Corey Colligan <coreyleec91288@gmail.com>
2021-03-26 01:09:49 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const names = [ "Lisa", "Kaitlin", "Jan" ];
+
+    function writeCards(names) {
+        // for (let i = 0; i < 10; i++)
+        // console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    let i = 0; 
+    while (i < names.length) {
+        console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        i++;
+    }
+    return names;
+}
+
+writeCards(names)
+   
